3b221b6b75fb3ef32f86f4ced4c1421906f7fd62
tzh <tzh2005t@163.com>
2024-03-10 00:45:02 +0800

    feat: add yaml config file

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import numpy as np
 import time
 from src.QRcode_videography_detection import QRcode
 from src.pnp import Pnp
+from src.yaml_loader import load_config
 
 if __name__ == '__main__':
       # draw.generate_data() for test
       detector = QRcode()
-      pnp = Pnp(0.5, np.array([[3666.666504, 0, 1920 / 2], [0, 3666.666504, 1080 / 2], [0, 0, 1]], dtype=np.double)) # TODO
+      pnp = Pnp(0.02, *load_config("tzh_cam.yaml")) # TODO
 
       while True:
             dataresult,inputresult=detector.detectcode()
