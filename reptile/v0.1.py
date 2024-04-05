605bd3372e363d2d0c20ece032abe67a75170ab6
herbet <2660135302@qq.com>
2024-04-05 20:43:51 +0800

    finish reptile

--- reptile/v0.1.py
+++ reptile/v0.1.py
@@ -1,46 +1,45 @@
 #-*encoding:utf-8-*
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 import requests
 import os
+import time
 
-#find / -name "msedgedriver" 2>/dev/null  查找msedgedriver路径
-# 设置webdriver路径
-driver_path = '/home/herbert/edgedriver/msedgedriver'
+driver = webdriver.Chrome()#用selenium库打开谷歌浏览器
+driver.maximize_window()
+driver.get('https://cn.bing.com/images/search?q=%E4%BA%8C%E7%BB%B4%E7%A0%81&form=HDRSC2&first=1&cw=1177&ch=789')
 
-# 创建webdriver对象
-driver = webdriver.Edge(executable_path=driver_path)
-
-# 访问网页
-driver.get('https://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&dyTabStr=MCwzLDEsMiw0LDYsNSw8LDcsOQ%3D%3D&word=%E4%BA%8C%E7%BB%B4%E7%A0%81')
-
-# 执行JavaScript代码
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# 等待页面加载
-driver.implicitly_wait(10)
-
-# 获取页面源代码
-content = driver.page_source
-
-# 使用BeautifulSoup解析页面源代码
-soup = BeautifulSoup(content, 'html.parser')
-
-# 找到所有的二维码图片
-img_tags = soup.find_all('img')
-
-# 创建一个文件夹来保存二维码图片
-if not os.path.exists('qrcodes'):
-    os.makedirs('qrcodes')
-
-# 下载所有的二维码图片
-for i, img_tag in enumerate(img_tags):
-    img_url = img_tag.get('src')
-    response = requests.get(img_url)
-    with open('qrcodes/qrcode{}.jpg'.format(i), 'wb') as f:
-        f.write(response.content)
-
-# 关闭浏览器
-driver.quit()
-
+img_dir = os.path.join(os.curdir, 'qrcodes')#qrcodes是文件夹名称
+'''创建文件夹'''
+if not os.path.isdir(img_dir):
+    os.mkdir(img_dir)
+    
+num=1
+for i in range(10):  # 滚动10次，你可以根据需要调整这个数字
+    #img_list = driver.find_elements(By.CLASS_NAME,'mimg')
+    img_list = driver.find_elements(By.XPATH,'//*[@class="img_cont hoff"]/img')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    img_path = []
+    for img in img_list:
+        path = img.get_attribute('src')
+        img_path.append(path)
+        
+    '''下载图片'''
+    for path in img_path:
+        img_name = 'qrcode' + str(num) + '.jpg'  # 图片名称
+        if path is None:
+            continue
+        num+=1
+        filepath = os.path.join(img_dir, img_name)
+        print('Saving image to:', filepath)
+        try:
+            resp = requests.get(path)
+            resp.raise_for_status()  # 如果状态码不是200，引发异常
+        except requests.RequestException as e:
+            print('Failed to download image:', e)
+            continue  # 跳过这个路径，开始下一次迭代
+        with open(filepath, 'wb') as f:
+            for chunk in resp.iter_content(1024):  # 限速写入图片
+                f.write(chunk)
+        print('Image saved.')
